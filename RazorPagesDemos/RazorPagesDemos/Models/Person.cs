ecd3242640aa27989b1e0dc8468652847fb2ca90
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-10 17:29:14 +0530

    Add files via upload

---
+++ RazorPagesDemos/RazorPagesDemos/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace RazorPagesDemos.Models
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+      
+    }
+}
